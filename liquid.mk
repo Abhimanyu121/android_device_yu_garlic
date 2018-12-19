2c313ca56e30173feaa219d7f451871de59aae5b
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2018-12-19 17:03:46 +0000

    added blobs to the list

--- liquid.mk
+++ liquid.mk
@@ -18,9 +18,9 @@
 $(call inherit-product, device/yu/garlic/full_garlic.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/stag/main.mk)
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
-PRODUCT_NAME := stag_garlic
+PRODUCT_NAME := liquid_garlic
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := garlic
 PRODUCT_GMS_CLIENTID_BASE := android-micromax

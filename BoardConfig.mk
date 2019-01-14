c40db144fa980804b6983e37d8a2cb50540b129f
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-14 14:10:08 +0530

    garlic: Specify we provide our own qti telephony jar
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,6 +219,9 @@ TARGET_RIL_VARIANT := caf
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/recovery/fstab.qcom
 
+# Telephony
+TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
+
 # SELinux
 # include device/qcom/sepolicy/sepolicy.mk
 # BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
